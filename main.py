54811a5d7ea127b5765b2e4f379811dc1bb60b17
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-08-09 19:02:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ if files == '':
     messagebox.showwarning("title", "msg")    #if file == ''
 
 print(files)    #files 리스트 값 출력
+
+with open(filenm, 'r', encoding='utf8') as f:
+  dat = f.readlines()
+
+l = [ x.strip() for x in dat.split(',') ]
+
+if 'print' in l:
+  print(l)
+
