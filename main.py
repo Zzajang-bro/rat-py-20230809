83ce9c61a134d52384c58e8e4d8ae660bf6d62c1
Jang HyeongJun <Zzajang.bro+github@gmail.com>
2023-08-09 18:28:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import os
 from tkinter import filedialog
 from tkinter import messagebox
 
-list_file = []                                          #파일 목록 담을 리스트 생성
+list_file = []                                        
 files = filedialog.askopenfilenames(initialdir="/",\
-                 title = "파일을 선택 해 주세요",\
-                    filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.csv","*csv")))
-#files 변수에 선택 파일 경로 넣기
+                title = "select your file",\
+                filetypes = (("*.xlsx","*xlsx"),("*.xls","*xls"),("*.csv","*csv")))
 
 if files == '':
-    messagebox.showwarning("경고", "파일을 추가 하세요")    #파일 선택 안했을 때 메세지 출력
+    messagebox.showwarning("title", "msg")    #if file == ''
 
 print(files)    #files 리스트 값 출력
